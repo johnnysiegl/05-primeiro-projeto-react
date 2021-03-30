269d9f18fcfc250ae7b87d1c8b52e9a5762b6903
johnny.junior <johnny.junior@pentacom.solutions>
2021-03-29 21:48:57 -0300

    Finish project for GithubRepository

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,49 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom'
 
+import api from '../../services/api'
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories)
+    } else {
+      return []
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+  })
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    if (!newRepo){
+      setInputError('Type author/name of repository.')
+      return
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
+
+      const repository = response.data
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+      setInputError('')
+    } catch (error) {
+      setInputError('Error repository search.')
+    }
+
+    
+  }
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input value={newRepo} onChange={e => setNewRepo(e.target.value)}  placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      { inputError && <Error>{inputError}</Error> }
+
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars.githubusercontent.com/u/1784198?v=4" alt="Danilo Vieira" />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
+      {repositories.map(repository => (
+          <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars.githubusercontent.com/u/1784198?v=4" alt="Danilo Vieira" />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars.githubusercontent.com/u/1784198?v=4" alt="Danilo Vieira" />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
